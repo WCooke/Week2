88de810d66ecbd361245d8195f26df0065b069fa
Owen Smith <owensmith0031@gmail.com>
2015-10-05 10:56:09 +0100

    Update MacVendorFinder.py

--- MacVendorFinder.py
+++ MacVendorFinder.py
@@ -1,4 +1,4 @@
 usermac = input ("What is your Mac Address?")
-import urllib2
-response = urllib2.urlopen('https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD')
+import urllib.request
+response = urllib.urlopen.urlsopen('https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD')
 html = response.read()
